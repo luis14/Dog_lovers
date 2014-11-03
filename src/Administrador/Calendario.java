e8fcc7ba26879672a48a00124c3d7c0def72e49c
luis14 <luispacheco14@live.com>
2014-11-03 16:33:59 -0600

    Clase calendario simplificada y util

--- src/Administrador/Calendario.java
+++ src/Administrador/Calendario.java
@@ -5,25 +5,19 @@
  */
 
 package Administrador;
-import java.util.Calendar;
+import java.util.*;
+import java.text.*;
 
 /**
  *
  * @author LuisAlonso
  */
 public class Calendario {
+    Date ahora = new Date();
+    DateFormat fecha_completa = DateFormat.getDateInstance(DateFormat.SHORT, new Locale("es"));
+        String fecha = fecha_completa.format(ahora);
     
-    public static void main(String[] args) {
-      Calendar calendario = Calendar.getInstance();
-      
-      int anio = calendario.get(Calendar.YEAR);
-      int mes = calendario.get(Calendar.MONTH);      // 0 a 11
-      int dia = calendario.get(Calendar.DAY_OF_MONTH);
-      int hora = calendario.get(Calendar.HOUR_OF_DAY);
-      int minuto = calendario.get(Calendar.MINUTE);
-      int segundo = calendario.get(Calendar.SECOND);
-   
-      System.out.printf("Ahora es %4d/%02d/%02d %02d:%02d:%02d\n",  
-          anio, mes+1, dia, hora, minuto, segundo);
-   }
+    public String getfecha(){
+        return fecha;
+}
 }
