b75ad6bb7ec280c0fc7dfa9ee80fce42ea6b87a8
miumiu997 <miuyinyong@yahoo.com>
2014-11-04 18:09:47 -0600

    Se hizo el modelo match

--- src/Administrador/ModeloTablas.java
+++ src/Administrador/ModeloTablas.java
@@ -8,6 +8,7 @@ public class ModeloTablas {
     private static DefaultTableModel modeloAdoptantes;  
     private static DefaultTableModel modeloUsuarios; 
     
+    
     public static DefaultTableModel tablaMascotas(){ 
         String titulos[] = {"ID", "Estado", "Color", "Tipo", "Tamanio", "Chip", "Raza"}; 
         String informacion[][] = {}; 
@@ -31,34 +32,78 @@ public class ModeloTablas {
         return modeloMascotas;
     } 
       
-       public static DefaultTableModel tablaMascotas(String TipoDeConsulta, String Consulta){ 
+       public static DefaultTableModel tablaMascotas(String Estado, String Color, String Tipo, String Tamanio, String Chip, String Raza){ 
         String titulos[] = {"ID","Estado", "Color", "Tipo", "Tamanio", "Chip", "Raza"}; 
         String informacion[][] = {}; 
         modeloMascotas = new DefaultTableModel(informacion, titulos);  
         //ArrayList<Mascota> tipodemascota = Administrador.ListaDeMascotas;
-        for(int i = 0; i < Administrador.ListaDeMascotas.size(); i++){ 
-            if(TipoDeConsulta.equals("Estado") && Administrador.ListaDeMascotas.get(i).getEstado().equals(Consulta)){
-            modeloMascotas.addRow(Administrador.ListaDeMascotas.get(i).arreglo()); 
+        for(int i = 0; i < Administrador.ListaDeMascotas.size(); i++){  
+            int contador = 0;
+            if(Administrador.ListaDeMascotas.get(i).getEstado().equals(Estado)){
+                contador++; 
             } 
-            if(TipoDeConsulta.equals("Color") && Administrador.ListaDeMascotas.get(i).getColor().equals(Consulta)){
-            modeloMascotas.addRow(Administrador.ListaDeMascotas.get(i).arreglo()); 
+            if(Administrador.ListaDeMascotas.get(i).getColor().equals(Color)){
+                contador++;
             }
-            if(TipoDeConsulta.equals("Tipo") && Administrador.ListaDeMascotas.get(i).getTipo().equals(Consulta)){
-            modeloMascotas.addRow(Administrador.ListaDeMascotas.get(i).arreglo()); 
+            if(Administrador.ListaDeMascotas.get(i).getTipo().equals(Tipo)){
+                contador++;
             } 
-            if(TipoDeConsulta.equals("Tamanio") && Administrador.ListaDeMascotas.get(i).getTamanio().equals(Consulta)){
-            modeloMascotas.addRow(Administrador.ListaDeMascotas.get(i).arreglo()); 
+            if(Administrador.ListaDeMascotas.get(i).getTamanio().equals(Tamanio)){
+                contador++; 
             } 
-            if(TipoDeConsulta.equals("Chip") && Administrador.ListaDeMascotas.get(i).getChip().equals(Consulta)){
-            modeloMascotas.addRow(Administrador.ListaDeMascotas.get(i).arreglo()); 
+            if(Administrador.ListaDeMascotas.get(i).getChip().equals(Chip)){
+                contador++; 
             }
-            if(TipoDeConsulta.equals("Raza") && Administrador.ListaDeMascotas.get(i).getRaza().equals(Consulta)){
-            modeloMascotas.addRow(Administrador.ListaDeMascotas.get(i).arreglo()); 
+            if(Administrador.ListaDeMascotas.get(i).getRaza().equals(Raza)){
+                contador++; 
+            }
+            if(contador == 6){
+                modeloMascotas.addRow(Administrador.ListaDeMascotas.get(i).arreglo());
             }
         }  
         
         return modeloMascotas;
         
+    }  
+       
+       public static DefaultTableModel tablaMatch(int id){ 
+        String titulos[] = {"ID", "Estado", "Color", "Tipo", "Tamanio", "Chip", "Raza"}; 
+        String informacion[][] = {}; 
+        modeloMascotas = new DefaultTableModel(informacion, titulos); 
+        String ColorDeMascotaMatch = Administrador.ListaDeMascotas.get(id).getColor(); 
+        String ChipDeMascotaMatch = Administrador.ListaDeMascotas.get(id).getChip(); 
+        String TamanioDeMascotaMatch = Administrador.ListaDeMascotas.get(id).getTamanio(); 
+        String TipoDeMascotaMatch = Administrador.ListaDeMascotas.get(id).getTipo(); 
+        String RazaDeMascotaMatch = Administrador.ListaDeMascotas.get(id).getRaza(); 
+        
+        for(int i = 0; i < Administrador.ListaDeMascotas.size(); i++){ 
+           int contador = 0;  
+           Boolean encontrado = false; 
+           if(Administrador.ListaDeMascotas.get(i).getEstado().equals("Encontrado")){ 
+               encontrado = true;
+           }
+           if(ColorDeMascotaMatch.equals(Administrador.ListaDeMascotas.get(i).getColor())){
+               contador++; 
+           } 
+           if(ChipDeMascotaMatch.equals(Administrador.ListaDeMascotas.get(i).getChip())){
+               contador++; 
+           } 
+           if(TipoDeMascotaMatch.equals(Administrador.ListaDeMascotas.get(i).getTipo())){
+               contador++; 
+           } 
+           if(TamanioDeMascotaMatch.equals(Administrador.ListaDeMascotas.get(i).getTamanio())){
+               contador++; 
+           } 
+           if(RazaDeMascotaMatch.equals(Administrador.ListaDeMascotas.get(i).getRaza())){
+               contador++; 
+           } 
+           if(contador >=3 && encontrado == true){ 
+              modeloMascotas.addRow(Administrador.ListaDeMascotas.get(i).arreglo());  
+           }
+            
+        } 
+        
+        return modeloMascotas;
     } 
     
     public static DefaultTableModel tablaCasaCuna(){  
@@ -71,23 +116,25 @@ public class ModeloTablas {
         return modeloCasaCuna;
     } 
     
-     public static DefaultTableModel tablaCasaCuna(String TipoDeConsulta, String Consulta){ 
+     public static DefaultTableModel tablaCasaCuna(String Tipo, String Tamanio, String Raza){ 
         String titulos[] = {"ID","Tipo", "Raza", "Tamanio", "NecesitaAlimentacion", "Telefono", "Requerimientos"};  
         String informacion[][] = {}; 
         modeloCasaCuna = new DefaultTableModel(informacion, titulos);  
-        for(int i = 0; i < Administrador.ListaDeCasaCuna.size(); i++){ 
-            if(TipoDeConsulta.equals("Tipo") && Administrador.ListaDeCasaCuna.get(i).getTipo().equals(Consulta)){
-            modeloCasaCuna.addRow(Administrador.ListaDeCasaCuna.get(i).arreglo()); 
+        for(int i = 0; i < Administrador.ListaDeCasaCuna.size(); i++){  
+            int contador = 0; 
+            if(Administrador.ListaDeCasaCuna.get(i).getTipo().equals(Tipo)){
+                contador++;
             } 
-            if(TipoDeConsulta.equals("Tamanio") && Administrador.ListaDeCasaCuna.get(i).getTamanio().equals(Consulta)){
-            modeloCasaCuna.addRow(Administrador.ListaDeCasaCuna.get(i).arreglo()); 
+            if(Administrador.ListaDeCasaCuna.get(i).getTamanio().equals(Tamanio)){
+                contador++;
             } 
-            if(TipoDeConsulta.equals("Raza") && Administrador.ListaDeCasaCuna.get(i).getRaza().equals(Consulta)){
-            modeloCasaCuna.addRow(Administrador.ListaDeCasaCuna.get(i).arreglo()); 
+            if(Administrador.ListaDeCasaCuna.get(i).getRaza().equals(Raza)){ 
+                contador++; 
             }  
-            if(TipoDeConsulta.equals("Raza") && Administrador.ListaDeCasaCuna.get(i).getNecesitaAlimentacion().equals(Consulta)){
-            modeloCasaCuna.addRow(Administrador.ListaDeCasaCuna.get(i).arreglo()); 
-            } 
+            if(contador == 3){
+                modeloCasaCuna.addRow(Administrador.ListaDeCasaCuna.get(i).arreglo());
+            }
+            
         } 
         return modeloCasaCuna;
     } 
