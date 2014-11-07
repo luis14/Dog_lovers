ceb58a94d660d8caf8a34c6166a03f7b21288857
Santiago Méndez <santiago1096@gmail.com>
2014-11-07 15:30:56 -0600

    - Modificaciones interfaz JUsuario

--- src/interfaz/JUsuario.java
+++ src/interfaz/JUsuario.java
@@ -24,7 +24,7 @@ public class JUsuario extends javax.swing.JFrame {
         UsuarioText = new javax.swing.JTextField();
         ContraText = new javax.swing.JPasswordField();
         ErrorLabel = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
+        registrarButton = new javax.swing.JButton();
         LogInButton = new javax.swing.JButton();
         jPanel2 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
@@ -56,14 +56,22 @@ public class JUsuario extends javax.swing.JFrame {
         ErrorLabel.setForeground(new java.awt.Color(204, 0, 0));
         ErrorLabel.setText("Contraseña o Usuario Incorrecto");
 
-        jButton1.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
-        jButton1.setForeground(new java.awt.Color(0, 153, 0));
-        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/1410309424_list-add-user.png"))); // NOI18N
-        jButton1.setText("Registrar");
-        jButton1.setToolTipText("");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        registrarButton.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        registrarButton.setForeground(new java.awt.Color(0, 153, 0));
+        registrarButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/1410309424_list-add-user.png"))); // NOI18N
+        registrarButton.setText("Registrar");
+        registrarButton.setToolTipText("");
+        registrarButton.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                registrarButtonMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                registrarButtonMouseExited(evt);
+            }
+        });
+        registrarButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                registrarButtonActionPerformed(evt);
             }
         });
 
@@ -104,7 +112,7 @@ public class JUsuario extends javax.swing.JFrame {
                     .addComponent(ErrorLabel))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jButton1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 129, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(registrarButton, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 129, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(LogInButton, javax.swing.GroupLayout.Alignment.TRAILING))
                 .addGap(27, 27, 27))
         );
@@ -114,7 +122,7 @@ public class JUsuario extends javax.swing.JFrame {
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(UsuarioLabel)
-                    .addComponent(jButton1)
+                    .addComponent(registrarButton)
                     .addComponent(UsuarioText, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(15, 15, 15)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -176,21 +184,27 @@ public class JUsuario extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_UsuarioTextActionPerformed
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void registrarButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_registrarButtonActionPerformed
         JRegistrarUsuario  registro = new JRegistrarUsuario();
         registro.setVisible(true);
         dispose();
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_registrarButtonActionPerformed
 
     private void LogInButtonMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_LogInButtonMouseEntered
-        // TODO add your handling code here:
         LogInButton.setFont(new java.awt.Font("Tahoma", 1, 13));
     }//GEN-LAST:event_LogInButtonMouseEntered
 
     private void LogInButtonMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_LogInButtonMouseExited
-        // TODO add your handling code here:
         LogInButton.setFont(new java.awt.Font("Tahoma", 1, 11));
     }//GEN-LAST:event_LogInButtonMouseExited
+
+    private void registrarButtonMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_registrarButtonMouseEntered
+        registrarButton.setFont(new java.awt.Font("Tahoma", 1, 13));
+    }//GEN-LAST:event_registrarButtonMouseEntered
+
+    private void registrarButtonMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_registrarButtonMouseExited
+        registrarButton.setFont(new java.awt.Font("Tahoma", 1, 11));
+    }//GEN-LAST:event_registrarButtonMouseExited
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel ContraLabel;
@@ -199,10 +213,10 @@ public class JUsuario extends javax.swing.JFrame {
     private javax.swing.JButton LogInButton;
     private javax.swing.JLabel UsuarioLabel;
     private javax.swing.JTextField UsuarioText;
-    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
+    private javax.swing.JButton registrarButton;
     // End of variables declaration//GEN-END:variables
 
     private boolean VerificaUsuario(){
